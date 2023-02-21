e3c0a2e5995849a65b3844aae69124043d3e6c2d
stanzi1626 <stansfield_alex@yahoo.com>
2023-02-21 14:39:14 +0000

    indeces in a set for sorting

--- Assignment_2/main.cpp
+++ Assignment_2/main.cpp
@@ -18,6 +18,16 @@
 #include <sstream>
 #include <vector>
 #include <cmath>
+#include <set>
+
+void print_data(std::vector<std::string> &course_name, std::vector<int> &course_code,
+                std::vector<float> &course_mark, std::set<int> &indeces)
+{
+    for (std::set<int>::iterator it = indeces.begin(); it != indeces.end(); it++)
+    {
+        std::cout << course_mark[*it] << " " << course_code[*it] << " " << course_name[*it] << std::endl;
+    }
+}
 
 template <typename T>
 std::ostream& operator << (std::ostream& out, const std::vector<T>& vec) {
@@ -47,10 +57,12 @@ int get_file_size()
     return size;
 }
 
-void read_file(std::vector<std::string> &course_name, std::vector<int> &course_code, std::vector<float> &course_mark)
+void read_file(std::vector<std::string> &course_name, std::vector<int> &course_code,
+               std::vector<float> &course_mark)
 {
     std::fstream file;
     file.open("courselist.dat");
+    int index = 0;
     while (!file.eof())
     {
         std::string line;
@@ -74,45 +86,74 @@ void read_file(std::vector<std::string> &course_name, std::vector<int> &course_c
     file.close();
 }
 
-void calculate_statistical_data(std::vector<float> &course_mark)
+void calculate_statistical_data(std::vector<float> &marks, std::set<int> &indeces)
 {
     float mean = 0;
     float standard_deviation = 0;
     float standard_error_of_mean = 0;
 
-    for (int i = 0; i < course_mark.size(); i++)
+    for (std::set<int>::iterator it = indeces.begin(); it != indeces.end(); it++)
     {
-        mean += course_mark[i];
+        mean += marks[*it];
     }
-    mean /= course_mark.size();
+    mean /= indeces.size();
 
-    for (int i = 0; i < course_mark.size(); i++)
+    for (std::set<int>::iterator it = indeces.begin(); it != indeces.end(); it++)
     {
-        standard_deviation += (course_mark[i] - mean) * (course_mark[i] - mean);
+        standard_deviation += pow(marks[*it] - mean, 2);
     }
-    standard_deviation /= course_mark.size();
+    standard_deviation /= indeces.size();
     standard_deviation = sqrt(standard_deviation);
 
-    standard_error_of_mean = standard_deviation / sqrt(course_mark.size());
+    standard_error_of_mean = standard_deviation / sqrt(indeces.size());
 
     std::cout << "Mean: " << mean << std::endl;
     std::cout << "Standard deviation: " << standard_deviation << std::endl;
-    std::cout << "Standard error on mean: " << standard_error_of_mean << std::endl;
+    std::cout << "Standard error of mean: " << standard_error_of_mean << std::endl;
+}
+
+void sort_year(std::vector<std::string> &course_name, std::vector<int> &course_code,
+               std::vector<float> &course_mark, std::set<int> &indeces, int year, int file_size)
+{
+    for (int i = 0; i < file_size; i++)
+    {
+        if (course_code[i] / 10000 == year)
+        {
+            indeces.insert(i);
+        }
+    }
+
+    print_data(course_name, course_code, course_mark, indeces);
+    calculate_statistical_data(course_mark, indeces);
 }
 
 int main()
 {
     int file_size = get_file_size();
 
-    std::vector<std::string> course_name;
-    std::vector<int> course_code;
-    std::vector<float> course_mark;
+    std::vector<std::string> root_course_name;
+    std::vector<int> root_course_code;
+    std::vector<float> root_course_mark;
+    std::set<int> indeces;
 
-    read_file(course_name, course_code, course_mark);
+    read_file(root_course_name, root_course_code, root_course_mark);
+
+    bool program_running = true;
+    do
+    {
+        std::cout << "Enter a year to sort: ";
+        int year;
+        std::cin >> year;
+
+        sort_year(root_course_name, root_course_code, root_course_mark, indeces, year, file_size);
+        break;
+    } while (program_running);
+    
+    
 
     // Debug
-    // std::cout << course_name << std::endl;
-    calculate_statistical_data(course_mark);
+    // std::cout << root_indeces << std::endl;
+    // calculate_statistical_data(course_mark);
 
     return 0;
 }
