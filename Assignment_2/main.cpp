48f8826a8334b17ffddb98361218901d98c270db
stanzi1626 <stansfield_alex@yahoo.com>
2023-02-22 16:56:41 +0000

    sorting implemented, need to incorportate with filter

--- Assignment_2/main.cpp
+++ Assignment_2/main.cpp
@@ -23,9 +23,15 @@
 void print_data(std::vector<std::string> &course_name, std::vector<int> &course_code,
                 std::vector<float> &course_mark, std::set<int> &indices)
 {
-    for (std::set<int>::iterator it = indices.begin(); it != indices.end(); ++it)
-    {
-        std::cout << course_mark[*it] << " " << course_code[*it] << " " << course_name[*it] << std::endl;
+    if (indices.size() == 0) {
+        for (unsigned int i = 0; i < course_name.size(); i++) {
+            std::cout << course_mark[i] << " " << course_code[i] << " " << course_name[i] << std::endl;
+        }
+    } else {
+        for (std::set<int>::iterator it = indices.begin(); it != indices.end(); ++it)
+        {
+            std::cout << course_mark[*it] << " " << course_code[*it] << " " << course_name[*it] << std::endl;
+        }
     }
 }
 
@@ -84,6 +90,7 @@ void read_file(std::vector<std::string> &course_name, std::vector<int> &course_c
         course_name.push_back(name);
     }
     file.close();
+    std::cout << "File read successfully" << std::endl;
 }
 
 void calculate_statistical_data(std::vector<float> &marks, std::set<int> &indices)
@@ -92,31 +99,41 @@ void calculate_statistical_data(std::vector<float> &marks, std::set<int> &indice
     float standard_deviation = 0;
     float standard_error_of_mean = 0;
 
-    for (std::set<int>::iterator it = indices.begin(); it != indices.end(); it++)
-    {
-        mean += marks[*it];
+    if (indices.size() == 0) {
+        for (unsigned int i = 0; i < marks.size(); i++) {
+            mean += marks[i];
+        }
+        mean /= marks.size();
+
+        for (unsigned int i = 0; i < marks.size(); i++) {
+            standard_deviation += pow(marks[i] - mean, 2);
+        }
+        standard_deviation /= marks.size();
+        standard_deviation = sqrt(standard_deviation);
+        standard_error_of_mean = standard_deviation / sqrt(marks.size());
+    } else {
+        for (std::set<int>::iterator it = indices.begin(); it != indices.end(); it++) {
+            mean += marks[*it];
+        }
+        mean /= indices.size();
+
+        for (std::set<int>::iterator it = indices.begin(); it != indices.end(); it++) {
+            standard_deviation += pow(marks[*it] - mean, 2);
+        }
+        standard_deviation /= indices.size();
+        standard_deviation = sqrt(standard_deviation);
+        standard_error_of_mean = standard_deviation / sqrt(indices.size());
     }
-    mean /= indices.size();
-
-    for (std::set<int>::iterator it = indices.begin(); it != indices.end(); it++)
-    {
-        standard_deviation += pow(marks[*it] - mean, 2);
-    }
-    standard_deviation /= indices.size();
-    standard_deviation = sqrt(standard_deviation);
-
-    standard_error_of_mean = standard_deviation / sqrt(indices.size());
-
-    std::cout << "Mean: " << mean << std::endl;
+    std::cout << std::endl << "Mean: " << mean << std::endl;
     std::cout << "Standard deviation: " << standard_deviation << std::endl;
-    std::cout << "Standard error of mean: " << standard_error_of_mean << std::endl;
+    std::cout << "Standard error of mean: " << standard_error_of_mean << std::endl << std::endl;
 }
 
-void sort_year(std::vector<std::string> &course_name, std::vector<int> &course_code,
+void filter_year(std::vector<std::string> &course_name, std::vector<int> &course_code,
                std::vector<float> &course_mark, std::set<int> &indices, int year, int file_size)
 {
     if (indices.size() == 0) {
-        for (int i = 0; i < file_size; i++) {
+        for (unsigned int i = 0; i < file_size; i++) {
             if (course_code[i] / 10000 == year) {
                 indices.insert(i);
             }
@@ -130,16 +147,13 @@ void sort_year(std::vector<std::string> &course_name, std::vector<int> &course_c
             }
         }
     }
-
-    print_data(course_name, course_code, course_mark, indices);
-    calculate_statistical_data(course_mark, indices);
 }
 
-void sort_mark(std::vector<std::string> &course_name, std::vector<int> &course_code,
+void filter_mark(std::vector<std::string> &course_name, std::vector<int> &course_code,
                std::vector<float> &course_mark, std::set<int> &indices, float mark, int file_size)
 {
     if (indices.size() == 0) {
-        for (int i = 0; i < file_size; i++) {
+        for (unsigned int i = 0; i < file_size; i++) {
             if (course_mark[i] >= mark) {
                 indices.insert(i);
             }
@@ -153,9 +167,141 @@ void sort_mark(std::vector<std::string> &course_name, std::vector<int> &course_c
             }
         }
     }
+}
 
-    print_data(course_name, course_code, course_mark, indices);
-    calculate_statistical_data(course_mark, indices);
+void sort_mark_into_descending_order(std::vector<std::string> &course_name, std::vector<int> &course_code,
+               std::vector<float> &course_mark, int file_size)
+{
+    for (unsigned int i = 0; i < file_size; i++) {
+        for (unsigned int j = i + 1; j < file_size; j++) {
+            if (course_mark[i] < course_mark[j]) {
+                std::swap(course_mark[i], course_mark[j]);
+                std::swap(course_code[i], course_code[j]);
+                std::swap(course_name[i], course_name[j]);
+            }
+        }
+    }
+}
+
+void sort_name_into_alphabetical_order(std::vector<std::string> &course_name, std::vector<int> &course_code,
+               std::vector<float> &course_mark, int file_size)
+{
+    for (unsigned int i = 0; i < file_size; i++) {
+        for (unsigned int j = i + 1; j < file_size; j++) {
+            if (course_name[i] > course_name[j]) {
+                std::swap(course_mark[i], course_mark[j]);
+                std::swap(course_code[i], course_code[j]);
+                std::swap(course_name[i], course_name[j]);
+            }
+        }
+    }
+}
+
+void year_check(int &year)
+{
+    do {
+        std::cin >> year;
+
+        if (year < 1 || year > 4 || std::cin.fail()) {
+            std::cout << "Please enter a valid year between 1-4" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            continue;
+        } else {
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            break;
+        }
+    } while (true);
+}
+
+void mark_check(float &mark)
+{
+    do {
+        std::cin >> mark;
+
+        if (mark < 0 || mark > 100 || std::cin.fail()) {
+            std::cout << "Please enter a valid mark between 0-100" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            continue;
+        } else {
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            break;
+        }
+    } while (true);
+}
+
+void print_menu()
+{
+    std::cout << std::endl << "1. Filter by year" << std::endl;
+    std::cout << "2. Filter by mark" << std::endl;
+    std::cout << "3. Sort name into alphabetical order" << std::endl;
+    std::cout << "4. Sort mark into descending order" << std::endl;
+    std::cout << "5. Print" << std::endl;
+    std::cout << "6. Reset" << std::endl;
+    std::cout << "7. Exit" << std::endl << std::endl;
+}
+
+int main_menu_check(int &menu_option)
+{
+    do
+    {
+        std::cin >> menu_option;
+
+        if (menu_option < 1 || menu_option > 7 || std::cin.fail()) {
+            std::cout << "Please enter a valid option" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            continue;
+        } else {
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            break;
+        }
+    } while (true);
+    return menu_option;
+}
+
+void main_menu_options(int menu_option, std::vector<std::string> &course_name, std::vector<int> &course_code,
+                       std::vector<float> &course_mark, std::set<int> &indices, int file_size)
+{
+    switch (menu_option) {
+    case 1:
+        std::cout << "Enter a year to sort (between 1-4): " << std::endl;
+        int year;
+        year_check(year);
+        filter_year(course_name, course_code, course_mark, indices, year, file_size);
+        break;
+    case 2:
+        std::cout << "Enter a mark to sort (between 0-100): " << std::endl;
+        float mark;
+        mark_check(mark);
+        filter_mark(course_name, course_code, course_mark, indices, mark, file_size);
+        break;
+    case 3:
+        sort_name_into_alphabetical_order(course_name, course_code, course_mark, file_size);
+        break;
+    case 4:
+        sort_mark_into_descending_order(course_name, course_code, course_mark, file_size);
+        break;
+    case 5:
+        print_data(course_name, course_code, course_mark, indices);
+        calculate_statistical_data(course_mark, indices);
+        break;
+    case 6:
+        indices.clear();
+        break;
+    case 7:
+        std::cout << "Exiting..." << std::endl;
+        break;
+    }
+}
+
+void print_divider()
+{
+    std::cout << "----------------------------------------" << std::endl;
 }
 
 int main()
@@ -169,29 +315,17 @@ int main()
 
     read_file(root_course_name, root_course_code, root_course_mark);
 
-    bool program_running = true;
-    do
-    {
-        std::cout << "Enter a year to sort (between 1-4): ";
-        int year;
-        std::cin >> year;
+    int menu_option;
+    while(true) {
+        print_divider();
+        print_menu();
+        main_menu_check(menu_option);
+        std::cout << "You picked option: " << menu_option << std::endl << std::endl;
+        main_menu_options(menu_option, root_course_name, root_course_code, root_course_mark, indices, file_size);
 
-        sort_year(root_course_name, root_course_code, root_course_mark, indices, year, file_size);
-
-        std::cout << "Enter a mark: ";
-        int mark;
-        std::cin >> mark;
-
-        sort_mark(root_course_name, root_course_code, root_course_mark, indices, mark, file_size);
-
-        break;
-    } while (program_running);
-    
-    
-
-    // Debug
-    // std::cout << root_indices << std::endl;
-    // calculate_statistical_data(course_mark);
-
+        if (menu_option == 7) {
+            break;
+        }
+    }
     return 0;
 }
